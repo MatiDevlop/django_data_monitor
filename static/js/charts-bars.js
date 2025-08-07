823838429d8241bef50d109727b2fee805c1b76a
Matias <kmmoroch@espol.edu.ec>
2025-08-07 16:26:32 -0500

    SSR con gráfico

--- static/js/charts-bars.js
+++ static/js/charts-bars.js
@@ -1,24 +1,32 @@
 /**
  * For usage, visit Chart.js docs https://www.chartjs.org/docs/latest/
  */
+
 const barConfig = {
   type: 'bar',
   data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: [ 'Junio', 'Julio', 'Agosto'],
     datasets: [
       {
-        label: 'Shoes',
+        label: 'Producto 1',
         backgroundColor: '#0694a2',
         // borderColor: window.chartColors.red,
         borderWidth: 1,
-        data: [-3, 14, 52, 74, 33, 90, 70],
+        data: [2, 1, 0],
       },
       {
-        label: 'Bags',
+        label: 'Producto 2',
         backgroundColor: '#7e3af2',
         // borderColor: window.chartColors.blue,
         borderWidth: 1,
-        data: [66, 33, 43, 12, 54, 62, 84],
+        data: [1, 0, 0],
+      },
+      {
+        label: 'Producto 3',
+        backgroundColor: 'rgba(88, 228, 118, 1)',
+        // borderColor: window.chartColors.blue,
+        borderWidth: 1,
+        data: [3, 0, 2],
       },
     ],
   },
