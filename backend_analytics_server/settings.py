26c577b4021e6789e54b0a428c2023a939289976
Matias <kmmoroch@espol.edu.ec>
2025-08-21 23:11:35 -0500

    Guía 25 para ver los estilos

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, STATIC_URL),
 ]
 
-STATIC_ROOT = BASE_DIR / 'assets'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
